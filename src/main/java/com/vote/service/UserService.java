cf47fba8747c4481c19e6b067ac20f4e4cb767ba
wtx1570345079 <1570345079@qq.com>
2019-07-08 17:22:54 +0800

    wtx7.8

--- src/main/java/com/vote/service/UserService.java
+++ src/main/java/com/vote/service/UserService.java
@@ -1,6 +1,9 @@
 package com.vote.service;
 
+import com.vote.pojo.ResultData;
 import com.vote.pojo.User;
+
+import java.util.List;
 
 public interface UserService {
     // 注册用户
@@ -9,4 +12,8 @@ public interface UserService {
     public int updateUser(User user);
     //删除用户
     public  int deleteUser(User user);
+    // 查询当前页用户
+    public ResultData findCurPage(int curPage,int pageSize);
+    // 查询用户总数
+    public  int  uerTotal();
 }
